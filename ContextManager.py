b0120abafe06a58d2fb3598cbd120bb88c4fe926
AzarovDN <azarovdn@icloud.com>
2018-12-01 18:51:03 +1000

    Решил задачу 2 вторым способом

--- ContextManager.py
+++ ContextManager.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+from contextlib import contextmanager
 import time
+
 
 
 class Time:
@@ -22,5 +24,27 @@ try:
 except ValueError as er:
     print('Вы ввели некоректные данные')
 
+
+# Вариант 2
+print('\n', 'Вариaнт 2', '\n')
+
+@contextmanager
+def delta_time():
+    a = datetime.now()
+    print(f'Время начала работы цикла {a}')
+
+    yield
+
+    b = datetime.now()
+    print(f'Время окончания работы цикла {b}')
+    print(f'Время работы цикла {b - a}')
+
+try:
+    counter = int(input('Введите количество итераций: '))
+    with delta_time():
+        for i in range(counter):
+            time.sleep(0.1)
+except ValueError as er:
+    print('Вы ввели некоректные данные')
 
 
